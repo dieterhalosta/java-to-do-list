4cf14721339e8371ae4b673c463b4841a3d0de6d
Dieter Halosta <dieter.halosta@yahoo.com>
2020-05-11 20:32:39 +0300

    Implemented CRUD methods in TaskRepository

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,23 +1,38 @@
 package org.fasttrackit;
 
+import com.mysql.cj.x.protobuf.MysqlxCrud;
+import org.fasttrackit.domain.Task;
 import org.fasttrackit.persistance.TaskRepository;
 import org.fasttrackit.transfer.CreateTaskRequest;
+import org.fasttrackit.transfer.UpdateTaskRequest;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.List;
 
 public class App
 {
     public static void main( String[] args ) throws IOException, SQLException, ClassNotFoundException {
+            // create a task
+//        CreateTaskRequest request = new CreateTaskRequest();
+//        request.setDescription("Learn JDBCTEST222");
+//        request.setDeadline(LocalDate.now().plusWeeks(1));
 
-        CreateTaskRequest request = new CreateTaskRequest();
-        request.setDescription("Learn JDBCTEST222");
-        request.setDeadline(LocalDate.now().plusWeeks(1));
+
+//        UpdateTaskRequest updateRequest = new UpdateTaskRequest();
+//        updateRequest.setDone(true);
+
 
         TaskRepository taskRepository = new TaskRepository();
-        taskRepository.createTask(request);
 
+        // update a task
+        //        taskRepository.updateTask(1, updateRequest);
+        // delete a task
+        //        taskRepository.deleteTask(4);
+        // get all tasks as a list
+        //        List<Task> tasks = taskRepository.getTasks();
+        //        System.out.println(tasks);
 
     }
 }
